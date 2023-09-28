097c404e894da08cf500fe95c99390c2deb9bf60
martinferrando2005 <ferrandomartin6@gmail.com>
2023-09-28 19:54:16 -0300

    styles modified in filters, and the component sidebar done for the sprint 2

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,34 +1,38 @@
-
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
+//styles
+import "./Reports.scss"
 //components
-import Table from './Tables/Table';
-import SingleView from '../../commons/SingleView/SingleView';
-import Filters from './filters/Filters';
+import Table from "./Tables/Table";
+import SingleView from "../../commons/SingleView/SingleView";
+import Filters from "./filters/Filters";
 
 const Reports = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [selectedReport, setSelectedReport] = useState(null);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedReport, setSelectedReport] = useState(null);
 
-    const handleOpenModal = (report) => {
-        setSelectedReport(report);
-        setModalIsOpen(true);
-    };
+  const handleOpenModal = (report) => {
+    setSelectedReport(report);
+    setModalIsOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setSelectedReport(null);
-        setModalIsOpen(false);
-    };
+  const handleCloseModal = () => {
+    setSelectedReport(null);
+    setModalIsOpen(false);
+  };
 
-
-    return (
-        <div>
-            <Filters />
-            <Table openModal={handleOpenModal} />
-            {modalIsOpen && <SingleView isOpen={modalIsOpen} onClose={handleCloseModal} selectedReport={selectedReport} />}
-        </div>
-    );
-
+   return (
+      <div className="container-reports">
+        <Filters />
+        <Table openModal={handleOpenModal} />
+        {modalIsOpen && (
+          <SingleView
+            isOpen={modalIsOpen}
+            onClose={handleCloseModal}
+            selectedReport={selectedReport}
+          />
+        )}
+      </div>
+  );
 };
 
 export default Reports;
