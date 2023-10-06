e47f1482cc0320f7381a5f5d902d5544e542de2a
martinferrando2005 <ferrandomartin6@gmail.com>
2023-10-05 21:05:23 -0300

    bug fixed in rowtable and filterButtons component

--- src/components/Reports/Tables/RowTable.jsx
+++ src/components/Reports/Tables/RowTable.jsx
@@ -1,86 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./rowTable.scss";
-import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
-import { dateFormater } from "../../../utils/dateFormater";
 import truncateFunctions from "../../../utils/truncateFunctions";
-import { specificAgentData } from "../../../utils/AgentsScreen/agents";
+import { useDispatch, useSelector } from "react-redux";
+
 
 const RowTable = ({ openModal }) => {
-  const paginatedItems = useSelector((store) => store.firestoreReducer.paginatedData);
+  const dispatch = useDispatch();
+
   const location = useLocation();
   const isConversationsView = location.pathname === "/conversations";
-
-  const agentsScreenData = specificAgentData(paginatedItems);
-  const arrayRenderToAgentsData = [];
-
-  const renderObjectWithAgentsProperties = () => {
-    for (const key in agentsScreenData) {
-      location.pathname != "/conversations"
-        ? arrayRenderToAgentsData.push(
-            <tr key={key} style={{ cursor: "auto" }}>
-              <td>{truncateFunctions(key)}</td>
-              <td className="bold center-text">
-                {agentsScreenData[key].totalInteractions}
-              </td>
-              <td className="center-text">
-                {agentsScreenData[key].promediumTotalRating + "%"}
-              </td>
-              <td className="center-text">
-                <p
-                  style={{ width: "50%" }}
-                  className={
-                    agentsScreenData[key].satisfactionPromediumIndex == "Bajo"
-                      ? "status-negative center-text"
-                      : agentsScreenData[key].satisfactionPromediumIndex ==
-                        "Medio"
-                      ? "status-medium center-text"
-                      : "status-positive center-text"
-                  }
-                >
-                  {agentsScreenData[key].satisfactionPromediumIndex}
-                </p>
-              </td>
-            </tr>
-          )
-        : "";
-    }
-  };
-
-  renderObjectWithAgentsProperties();
+  const { paginatedData } =
+    useSelector((store) => store.firestoreReducer);
 
   return (
     <tbody>
-      {isConversationsView
-        ? paginatedItems &&
-          paginatedItems?.map((el, i) => {
+      {isConversationsView && paginatedData
+        ? paginatedData &&
+          paginatedData?.map((el, i) => {
             return (
               <tr key={i} onClick={() => openModal(el)}>
-                <td className="item-date">{el.dateFormated}</td>
-                <td className="item-rating">{el.client.rating}%</td>
+                <td className="item-date">{el?.dateFormated}</td>
+                <td className="item-rating">{el?.client?.rating}%</td>
                 <td>
                   <p
                     className={
-                      parseInt(el.client.rating) < 40
+                      parseInt(el.client?.rating) < 40
                         ? "status-negative"
-                        : parseInt(el.client.rating) >= 40 &&
-                          parseInt(el.client.rating) < 70
+                        : parseInt(el.client?.rating) >= 40 &&
+                          parseInt(el.client?.rating) < 70
                         ? "status-medium"
                         : "status-positive"
                     }
                   >
-                    {el.client.satisfaction_index}
+                    {el.client?.satisfaction_index}
                   </p>
                 </td>
-                <td style={{paddingLeft: "6%"}}>{el.client.phone_number}</td>
-                <td>{el.client.dni}</td>
-                <td>{truncateFunctions(el.agent)}</td>
+                <td style={{ paddingLeft: "6%" }}>{el.client?.phone_number}</td>
+                <td>{el.client?.dni}</td>
+                <td>{el.agent && truncateFunctions(el.agent)}</td>
               </tr>
             );
           })
-        : arrayRenderToAgentsData.length &&
-          arrayRenderToAgentsData.map((el, i) => {
-            return el;
+        : paginatedData?.map((agent, i) => {
+            return (
+              <tr key={i} style={{ cursor: "auto" }}>
+                <td>
+                  {agent?.agentName && truncateFunctions(agent?.agentName)}
+                </td>
+                <td className="bold center-text">{agent.totalInteractions}</td>
+                <td className="center-text">
+                  {agent.promediumTotalRating + "%"}
+                </td>
+                <td className="center-text">
+                  <p
+                    style={{ width: "50%" }}
+                    className={
+                      agent.satisfactionPromediumIndex == "Bajo"
+                        ? "status-negative center-text"
+                        : agent.satisfactionPromediumIndex == "Medio"
+                        ? "status-medium center-text"
+                        : "status-positive center-text"
+                    }
+                  >
+                    {agent.satisfactionPromediumIndex}
+                  </p>
+                </td>
+              </tr>
+            );
           })}
     </tbody>
   );
