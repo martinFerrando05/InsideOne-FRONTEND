e3bba872bff63b8b2026284bfc88558fd9680550
Nicolas-David-Faure <faureee89@gmail.com>
2023-10-10 10:59:31 -0300

    analyze all the coversation when the chat is finished

--- src/store/slice/demoEmotionAnalysisSlice/demoEmotionAnalysisSlice.js
+++ src/store/slice/demoEmotionAnalysisSlice/demoEmotionAnalysisSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 
 export const demoEmotionAnalysisSlice = createSlice({
   name: "demoEmotionAnalisys",
@@ -6,38 +7,45 @@ export const demoEmotionAnalysisSlice = createSlice({
   initialState: {
     singleEmotionAnalysis: null,
     allSingleAnalysis: [],
-    conversation: [{
-      role: 'system',
-      content: 'Eres un asistente por chat de Galicia Seguros que brinda atención al cliente por chat para dar respuesta a preguntas sobre seguros. Podes dar información sobre tipos de seguros, hacer cotizaciones, ayudarte con trámites y responder preguntas comunes. Es como hablar con un experto en seguros en línea para obtener ayuda rápida y fácil.'
-    }],
+    conversation: [
+      {
+        role: "system",
+        content:
+          "Eres un asistente por chat de Galicia Seguros que brinda atención al cliente por chat para dar respuesta a preguntas sobre seguros. Podes dar información sobre tipos de seguros, hacer cotizaciones, ayudarte con trámites y responder preguntas comunes. Es como hablar con un experto en seguros en línea para obtener ayuda rápida y fácil.",
+      },
+    ],
     allConversationAnalyis: null,
-    mode: 'singleMessage',
+    mode: "singleMessage",
     loadingConversation: false,
-    loadingAnalysis: false
+    loadingAnalysis: false,
   },
   reducers: {
     setCoversation: (state, { payload }) => {
-      state.conversation.push(payload)
-      state.loadingConversation = false
+      state.conversation.push(payload);
+      state.loadingConversation = false;
     },
     setSingleEmotionAnalisys: (state, { payload }) => {
       state.singleEmotionAnalysis = payload;
-      state.loadingAnalysis = false
+      state.loadingAnalysis = false;
     },
-    setMode :(state, { payload})=>{
-      state.mode = payload
+    setMode: (state, { payload }) => {
+      state.mode = payload;
     },
-    setLoadingConversation : (state, { payload})=>{
-      state.loadingConversation = payload
-    }
+    setLoadingConversation: (state, { payload }) => {
+      state.loadingConversation = payload;
+    },
 
-    ,setLoadingAnalysis: (state, { payload})=>{
-      state.loadingAnalysis = payload
-    }
-
-    
+    setLoadingAnalysis: (state, { payload }) => {
+      state.loadingAnalysis = payload;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { setSingleEmotionAnalisys  , setCoversation ,setMode , setLoadingConversation ,setLoadingAnalysis} = demoEmotionAnalysisSlice.actions
+export const {
+  setSingleEmotionAnalisys,
+  setCoversation,
+  setMode,
+  setLoadingConversation,
+  setLoadingAnalysis,
+} = demoEmotionAnalysisSlice.actions;
